fff4e43bd0d24a70a7ffb965ac428ce1283a4ac1
arturoromeroum <ajruwork@gmail.com>
2020-05-25 18:45:27 -0500

    se agregaron comandos SQL en el archivo model.py, para crear la tabla libros, la cual fue modificada en el tipo de dato del id_libros, se agregaron tambien los comandos para crear las tablas prestamos, usuarios, editorial, titulo, autor

--- modelos.py
+++ modelos.py
@@ -18,7 +18,7 @@ class ConnectionDB:
         except Error as e:
             write_errors(e, 'Ocurrio un error al conectarse a la base de datos')
 
-###### EJECUTAR CODIGO SQL ######
+################ EJECUTAR CODIGO SQL ################
     def _ejecutar_sql(
            self, sentencia_sql, param=None, 
            escribir_en_db=True
@@ -32,19 +32,81 @@ class ConnectionDB:
            if escribir_en_db:
                    self.db.rollback()
 
-###### CREAR TABLAS ######
-    def crear_tabla(self):  
-           self._ejecutar_sql(
-               """
-               CREATE TABLE libros(
-                   id_book SERIAL,
-                   title_id INT,
-                   status INT,
-                   editorial_id INT,
-                   author_id INT,
-                   PRIMARY KEY (id_book)
-               )
-               """
-           )
+################ CREAR TABLAS ################
+    def crear_tabla(self):
+        
+        ##### TABLA LIBROS #####
+        self._ejecutar_sql(
+            """
+            CREATE TABLE libros(
+                id_book INT NOT NULL,
+                title_id INT NOT NULL,
+                status INT,
+                editorial_id INT NOT NULL,
+                author_id INT NOT NULL,
+                PRIMARY KEY (id_book)
+            )
+            """
+        )
+           
+        ##### TABLA PRESTAMOS #####
+        self._ejecutar_sql(
+            """
+            CREATE TABLE loans(
+                id SERIAL,
+                retal_date INT,
+                return_date INT,
+                id_book INT,
+                status INT,
+                user_id INT NOT NULL,
+                PRIMARY KEY (id)
+            )
+            """
+        )
+
+        ##### TABLA USUARIOS #####
+        self._ejecutar_sql(
+            """
+            CREATE TABLE user(
+                user_id SERIAL,
+                name VARCHAR(50) NOT NULL,
+                dni INT NOT NULL,
+                PRIMARY KEY (user_id)
+            )
+            """
+        )
+
+        ##### TABLA EDITORIAL #####
+        self._ejecutar_sql(
+            """
+            CREATE TABLE editorial(
+                editorial_id SERIAL,
+                editorial VARCHAR(50) NOT NULL,
+                PRIMARY KEY (editorial_id)
+            )
+            """
+        )
+
+        ##### TABLA TITULO #####
+        self._ejecutar_sql(
+            """
+            CREATE TABLE title(
+                title_id SERIAL,
+                title VARCHAR NOT NULL,
+                PRIMARY KEY (editorial_id)
+            )
+            """
+        )
+
+        ##### TABLA AUTOR #####
+        self._ejecutar_sql(
+            """
+            CREATE TABLE author(
+                author_id SERIAL,
+                author VARCHAR(50) NOT NULL,
+                PRIMARY KEY (editorial_id)
+            )
+            """
+        )
 
 
