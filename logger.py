cdb358c3ffc3eb8cec4edd9130f64bb2856be255
Arturo Romero <61916588+arturoromerou@users.noreply.github.com>
2020-05-25 16:20:02 -0500

    delete PNG
    el PNG fue modificado por otro archivo

--- logger.py
+++
@@ -1,6 +0,0 @@
-def write_errors(exception, text):
-    with open('error.log', 'a+') as log_archive:
-        log_archive.write('----------------------------------------------------------------\n')
-        log_archive.write(str(exception))
-        log_archive.write(text)
-        log_archive.write('----------------------------------------------------------------\n')
