de31792a794ed23818423d29ede6bd62600a997d
arturoromeroum <ajruwork@gmail.com>
2020-05-25 18:11:57 -0500

    se agregaron comandos SQL en el archivo model.py, para crear la tabla libros

--- modelos.py
+++ modelos.py
@@ -11,8 +11,40 @@ class ConnectionDB:
             self.db = connect(
                 host=param['host'],
                 user=param['user'],
-                password=param['password']
+                password=param['password'],
+                database=param['database']
             )
             self.cursor = self.db.cursor()
         except Error as e:
             write_errors(e, 'Ocurrio un error al conectarse a la base de datos')
+
+###### EJECUTAR CODIGO SQL ######
+    def _ejecutar_sql(
+           self, sentencia_sql, param=None, 
+           escribir_en_db=True
+       ):
+           try:
+               self.cursor.execute(sentencia_sql, param) # execute corre las sentencias sql
+               if escribir_en_db:
+                   self.db.commit()
+           except Exception as e:
+                write_errors(e, f"Ocurrio un error al ejecutar la sentencia SQL:\n\n{sentencia_sql}\n")
+           if escribir_en_db:
+                   self.db.rollback()
+
+###### CREAR TABLAS ######
+    def crear_tabla(self):  
+           self._ejecutar_sql(
+               """
+               CREATE TABLE libros(
+                   id_book SERIAL,
+                   title_id INT,
+                   status INT,
+                   editorial_id INT,
+                   author_id INT,
+                   PRIMARY KEY (id_book)
+               )
+               """
+           )
+
+
