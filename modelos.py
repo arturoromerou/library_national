f0ecf344275c77468f15513cb4383124ed2d7924
arturoromeroum <ajruwork@gmail.com>
2020-05-29 22:31:40 -0500

    se modificaron los metodos de conexion a la bs y se agrego una clase para crear los datos

--- modelos.py
+++ modelos.py
@@ -1,39 +1,45 @@
+"""App models.""" 
 from psycopg2 import connect, Error
 from logger import write_errors
 
+
 class ConnectionDB:
+    """Connection class."""
     
     bd = None
     cursor = None
 
     def __init__(self, **param):
+        """Connection constructor."""
         try:
             self.db = connect(
-                host=param['host'],
-                user=param['user'],
-                password=param['password'],
-                database=param['database']
+                host = '127.0.0.1', # localhost
+                user = 'postgres',  # usuario de la bd
+                password = 'Ar2098urd',  # contrasenia
+                database = 'biblioteca_nacional'  # nombre de la bd
             )
             self.cursor = self.db.cursor()
         except Error as e:
             write_errors(e, 'Ocurrio un error al conectarse a la base de datos')
 
-################ EJECUTAR CODIGO SQL ################
     def ejecutar_sql(
-           self, sentencia_sql, param=None, 
-           escribir_en_db=True
-       ):
-           try:
-               self.cursor.execute(sentencia_sql, param) # execute corre las sentencias sql
-               if escribir_en_db:
-                   self.db.commit()
-           except Exception as e:
-                write_errors(e, f"Ocurrio un error al ejecutar la sentencia SQL:\n\n{sentencia_sql}\n")
-           if escribir_en_db:
-                   self.db.rollback()
+        self, 
+        sentencia_sql, 
+        param=None, 
+        escribir_en_db=True
+    ):
+        try:
+            execute = self.cursor.execute(sentencia_sql, param) # execute corre las sentencias sql
+            if escribir_en_db:
+                result =  self.db.commit()
+        except Exception as e:
+            write_errors(e, f"Ocurrio un error al ejecutar la sentencia SQL:\n\n{sentencia_sql}\n")
+            if escribir_en_db:
+                self.db.rollback()
 
-################ LEER CODIGO SQL ################
+
     def leer_desde_sql(self):
+        """Leer codigo sql."""
         try:
             registros = self.cursor.fetchall()
             for x in registros:
@@ -42,17 +48,18 @@ class ConnectionDB:
             write_errors(e, f'Un error ocurrió al momento de leer desde la BD')
         return registros
 
-################ CREAR TABLAS ################
+
 class Tabla(ConnectionDB):
+    """Crear tablas."""
 
     def crear_tabla(self):
         
-        ##### TABLA LIBROS #####
+        # TABLA LIBROS
         self.ejecutar_sql(
             """
-            CREATE TABLE books(
+            CREATE TABLE IF NOT EXISTS books(
                 id_book INT NOT NULL,
-                title_id INT NOT NULL,
+                title VARCHAR(255) NOT NULL,
                 status INT,
                 editorial_id INT NOT NULL,
                 author_id INT NOT NULL,
@@ -61,25 +68,24 @@ class Tabla(ConnectionDB):
             """
         )
            
-        ##### TABLA PRESTAMOS #####
+        # TABLA PRESTAMOS
         self.ejecutar_sql(
             """
-            CREATE TABLE loans(
+            CREATE TABLE IF NOT EXISTS rent(
                 id SERIAL,
-                rental_date INT,
-                return_date INT,
+                start_date DATE,
+                end_date DATE,
                 id_book INT,
-                status INT,
                 user_id INT NOT NULL,
                 PRIMARY KEY (id)
             )
             """
         )
 
-        ##### TABLA USUARIOS #####
+        # TABLA USUARIOS
         self.ejecutar_sql(
             """
-            CREATE TABLE user(
+            CREATE TABLE IF NOT EXISTS user(
                 user_id SERIAL,
                 name VARCHAR(50) NOT NULL,
                 dni INT NOT NULL,
@@ -88,129 +94,112 @@ class Tabla(ConnectionDB):
             """
         )
 
-        ##### TABLA EDITORIAL #####
+        # TABLA EDITORIAL
         self.ejecutar_sql(
             """
-            CREATE TABLE editorial(
+            CREATE TABLE IF NOT EXISTS editorial(
                 editorial_id SERIAL,
-                editorial VARCHAR(50) NOT NULL,
+                name VARCHAR(50) NOT NULL,
                 PRIMARY KEY (editorial_id)
             )
             """
         )
 
-        ##### TABLA TITULO #####
-        self.ejecutar_sql(
-            """
-            CREATE TABLE title(
-                title_id SERIAL,
-                title VARCHAR NOT NULL,
-                PRIMARY KEY (editorial_id)
-            )
-            """
-        )
-
-        ##### TABLA AUTOR #####
+        # TABLA AUTOR
         self.ejecutar_sql(
             """
             CREATE TABLE author(
                 author_id SERIAL,
-                author VARCHAR(50) NOT NULL,
+                name VARCHAR(50) NOT NULL,
                 PRIMARY KEY (editorial_id)
             )
             """
         )
 
-################ INSERTAR A LAS TABLAS ################
-class RegistrarLibro(ConnectionDB):
 
-    def insertar_editorial(self, editorial):
-        self.ejecutar_sql(
-            """INSERT INTO editorial (editorial) 
-            VALUES (%s,)""",
-            (editorial)
-    )
+class Model():
+    """Generic model class."""
 
-    def insertar_titulo(self, title):
-        self.ejecutar_sql(
-            """INSERT INTO title (title) 
-            VALUES (%s,)""",
-            (title)
-    )
+    table_name = None
+    connection = ConnectionDB()  # Esto no se hace
 
-    def insertar_autor(self, author):
-        self.ejecutar_sql(
-            """INSERT INTO author (author) 
-            VALUES (%s,)""",
-            (author)
-    )
+    def create(self):
+        """Guarda en base de datos."""
+        table_name = self.table_name
+        keys = ", ".join(self.__dict__.keys())
+        values_placeholders = ", ".join(["%s" for i in range(len(self.__dict__.keys()))])
+        values = self.__dict__.values()
+        sql = f"INSERT INTO {table_name} ({keys}) VALUES ({values_placeholders})"
+        
+        self.connection.ejecutar_sql(sql, tuple(values))
 
-    def insertar_libro(self, id_book):
-        self.ejecutar_sql(
-            """INSERT INTO books (id_book)
-            VALUES (%s,)""",
-            (id_book)
-        )
+    # TODO
+    def read(self):
+        """Obtiene un elemento de la base de datos."""
+        pass
 
-class RegistrarUsuario(ConnectionDB):
+    # TODO
+    def update(self):
+        """Actualiza un elemento de la base de datos."""
+        pass
 
-    def insertar_usuario(self, name, dni):
-        self.ejecutar_sql(
-            """INSERT INTO loans (name, dni) 
-            VALUES (%s, %s)""",
-            (name, dni)
-        )
+    # TODO
+    def delete(self):
+        """Borra un elemento de la base de datos."""
+        pass
 
-class RegistrarPrestamo(ConnectionDB):
+
+
+class Editorial(Model):
+    """Clase Editorial."""
+
+    table_name = "editorial"
+
+    name = None
+
+    def __init__(self, name):
+        """Método constructor."""
+        self.name = name
+
+
+
+class Autor(Model):
+    """Clase Autor"""
+
+    table_name = "author"
+
+    name = None
+
+    def __init__(self, name):
+        """Metodo Constructor"""
+        self.name = name
+
+class Libro(Model):
+    """Clase Libro"""
     
-    def ingresar_prestamo(self, rental_date, return_date, id_book, user_id):
-        self.ejecutar_sql(
-            """INSERT INTO loans (rental_date, return_date, id_book, user_id) 
-            VALUES (%s, %s, %s, %s)""",
-            (rental_date, return_date, id_book, user_id)
-        )
+    table_name = "books"
+    title = None
+    author_id = None
+    editorial_id = None
+    status = None
 
-################ VER LOS DATOS DE LAS TABLAS ################
-class Obtener(ConnectionDB):
+    def __init__(self, title, author_id, editorial_id, status):
+        """Metodo Constructor"""
+        self.title = title
+        self.author_id = author_id
+        self.editorial_id = editorial_id
+        self.status = status
 
-    def ver_libros(self):
-        self.ejecutar_sql(
-            "SELECT * FROM books",
-            escribir_en_db=False
-        )
-        self.leer_desde_sql()
 
-    def ver_usuarios(self):
-        self.ejecutar_sql(
-            "SELECT * FROM user",
-            escribir_en_db=False
-        )
-        self.leer_desde_sql()
+# TODO: Terminar este modelo
+class Usuario(Model):
+    """Clase usuario."""
 
-    def ver_prestamos(self):
-        self.ejecutar_sql(
-            "SELECT * FROM loans",
-            escribir_en_db=False
-        )
-        self.leer_desde_sql()
+    pass
 
-################ ELIMINAR DATOS DE LAS TABLAS ################
-class Eliminar(ConnectionDB):
 
-    def eliminar_libro(self, id_book):
-        self.ejecutar_sql(
-            "DELETE FROM books WHERE id_book=%s",
-            (id_book,)
-        )
-    
-    def eliminar_usuario(self, user_id):
-        self.ejecutar_sql(
-            "DELETE FROM user WHERE user_id=%s",
-            (user_id,)
-        )
+# TODO: Terminar este modelo
+class Rent(Model):
+    """Clase alquiler."""
 
-    def eliminar_registro(self, id):
-        self.ejecutar_sql(
-            "DELETE FROM loans WHERE id=%s",
-            (id)
-        )   
+    pass
